60470566d7fdfd4598ba650b060adae542eb6942
Luci02 <luciparlato50@gmail.com>
2023-06-26 15:18:39 +0200

    first commit

---
+++ Lezione_1/src/com/epicode/MainProject.java
@@ -0,0 +1,15 @@
+package com.epicode;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean b = true; // true || false
+		char c = 'c'; // apici singoli
+		int i = 25;
+		double d = 25.5;
+		String s = "ciao"; // apici doppi
+	}
+
+}
