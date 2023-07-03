567e09256d869109db66d075965d3e0ff8f30e9a
Luci02 <luciparlato50@gmail.com>
2023-07-03 23:46:42 +0200

    predisposizione svolgimento esercizio #3

---
+++ ExceptionHandling_EsercizioSerale/src/com/epicode/esercizio3/ContoCorrente.java
@@ -0,0 +1,28 @@
+package com.epicode.esercizio3;
+
+public class ContoCorrente {
+	
+	String titolare;
+	int nMovimenti;
+	final int maxMovimenti = 50;
+	double saldo;
+
+	public ContoCorrente(String titolare, double saldo) {
+		this.titolare = titolare;
+		this.saldo = saldo;
+		nMovimenti = 0;
+	}
+
+	public void preleva(double x) {
+		if (nMovimenti < maxMovimenti)
+			saldo = saldo - x;
+		else
+			saldo = saldo - x - 0.50;
+		nMovimenti++;
+	}
+
+	public double restituisciSaldo() {
+		return saldo;
+	}
+
+}
