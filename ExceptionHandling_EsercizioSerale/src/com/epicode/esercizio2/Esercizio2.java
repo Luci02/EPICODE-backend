a90dc86cc29a291fbee908480f98121acb2a3105
Luci02 <luciparlato50@gmail.com>
2023-07-03 23:31:03 +0200

    predisposizione svolgimento esercizio #2
    Creato il package apposito e le classi MainClass e Esercizio2 con metodo statico start()

---
+++ ExceptionHandling_EsercizioSerale/src/com/epicode/esercizio2/Esercizio2.java
@@ -0,0 +1,11 @@
+package com.epicode.esercizio2;
+
+public class Esercizio2 {
+	
+	public static void start() {
+		
+		
+		
+	}
+
+}
---
+++ ExceptionHandling_EsercizioSerale/src/com/epicode/esercizio2/MainClass.java
@@ -0,0 +1,11 @@
+package com.epicode.esercizio2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Esercizio2.start();
+
+	}
+
+}
