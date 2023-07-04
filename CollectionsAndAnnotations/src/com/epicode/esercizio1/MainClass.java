7ce525fbe9159d41793c3e7fa8e74595adabf9d9
Luci02 <luciparlato50@gmail.com>
2023-07-04 15:39:06 +0200

    completato esercizio #1
    Programma che chiede all'utente di inserire un numero intero N che rappresenti il numero di elementi da inserire e poi gli chieda di inserire N parole. Il programma stampa tutte le parole duplicate, il numero delle parole distinte e l'elenco delle parole distinte usando l'interfaccia Set e l'implementazione HashSet

--- CollectionsAndAnnotations/src/com/epicode/esercizio1/MainClass.java
+++ CollectionsAndAnnotations/src/com/epicode/esercizio1/MainClass.java
@@ -1,11 +1,57 @@
 package com.epicode.esercizio1;
 
-public class MainClass {
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass {
+	
+	@SuppressWarnings("unused")
+	private static final Logger log = LoggerFactory.getLogger(MainClass.class);
+	
 	public static void main(String[] args) {
 		
-		
+		start();
 
+	}
+	
+	private static void start() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Set<String> paroleDistinte = new HashSet<String>();
+		Set<String> paroleDuplicate = new HashSet<String>();
+		
+		try {
+			System.out.print("Digita il numero di elementi da inserire: ");
+			int n = Integer.parseInt(sc.nextLine());
+			
+			for (int i = 0; i < n; i++) {
+				System.out.print("Inserisci la parola n." + (i+1) + ": ");
+				String elem = sc.nextLine();
+				
+				if( !paroleDistinte.contains(elem) ) {				
+					paroleDistinte.add(elem);
+				}else {
+					paroleDuplicate.add(elem);
+				}
+			}
+			
+			if( !paroleDuplicate.isEmpty() ) {				
+				log.info( "Parole duplicate: " + paroleDuplicate.toString() );
+			}else {
+				log.info("Non ci sono parole duplicate");
+			}
+			
+			log.info( "Numero di parole distinte: " + paroleDistinte.size() );
+			log.info( "Parole distinte: " + paroleDistinte.toString() );
+			
+		} catch (NumberFormatException e) {
+			log.error("Inserisci il tipo di dato corretto!");
+		}
 	}
 
 }
