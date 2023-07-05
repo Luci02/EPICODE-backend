d6db6a8be18092da439e36d2fac57bce148ee2ef
Luci02 <luciparlato50@gmail.com>
2023-07-05 14:14:58 +0200

    Compito pomeridiano 05/07/2023
    Inizializzazione progetto Java + creato il package per lo svolgimento del primo esercizio

---
+++ Threads&FileHandling/src/com/epicode/esercizio1/MainClass.java
@@ -0,0 +1,15 @@
+package com.epicode.esercizio1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass {
+	
+	private static Logger log = LoggerFactory.getLogger(MainClass.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Threads&FileHandling/src/com/epicode/esercizio1/MyThread.java
@@ -0,0 +1,15 @@
+package com.epicode.esercizio1;
+
+public class MyThread extends Thread {
+	
+	private String symbol;
+	
+	public MyThread(String symbol) {
+		this.symbol = symbol;
+	}
+	
+	@Override
+	public void run() {
+	}
+
+}
