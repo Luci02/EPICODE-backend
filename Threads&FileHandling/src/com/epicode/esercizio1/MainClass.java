3f010456a216eeb18aedc79170f7a4a32a8d64d3
Luci02 <luciparlato50@gmail.com>
2023-07-05 14:22:06 +0200

    Compito pomeridiano 05/07/2023
    Completato esercizio #1
    
    Creata applicazione con due thread in cui ciascun thread ha associato un simbolo. Ogni Thread stampa su console il proprio simbolo attendendo 1 secondo tra una stampa e l'altra

--- Threads&FileHandling/src/com/epicode/esercizio1/MainClass.java
+++ Threads&FileHandling/src/com/epicode/esercizio1/MainClass.java
@@ -8,8 +8,11 @@ public class MainClass {
 	private static Logger log = LoggerFactory.getLogger(MainClass.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		MyThread t1 = new MyThread("*");
+		MyThread t2 = new MyThread("#");
+		
+		t1.start();
+		t2.start();
 	}
 
 }
--- Threads&FileHandling/src/com/epicode/esercizio1/MyThread.java
+++ Threads&FileHandling/src/com/epicode/esercizio1/MyThread.java
@@ -1,8 +1,12 @@
 package com.epicode.esercizio1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MyThread extends Thread {
 	
 	private String symbol;
+	private static Logger log = LoggerFactory.getLogger(MainClass.class);
 	
 	public MyThread(String symbol) {
 		this.symbol = symbol;
@@ -10,6 +14,15 @@ public class MyThread extends Thread {
 	
 	@Override
 	public void run() {
+		for (int i = 0; i < 10; i++) {
+			try {
+				System.out.println( (i+1) + ". " + this.symbol );
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				log.error( e.getMessage() );
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
