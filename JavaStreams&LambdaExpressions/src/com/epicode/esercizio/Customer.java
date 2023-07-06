bf996b763cab92c9ad4d7dc73d6a8966f9415d1f
Luci02 <luciparlato50@gmail.com>
2023-07-06 14:01:24 +0200

    Esercizio pomeridiano 06/07/2023
    Impostato il progetto Java con relativo package e creazione di 3 classi Product, Order e Customer

---
+++ JavaStreams&LambdaExpressions/src/com/epicode/esercizio/Customer.java
@@ -0,0 +1,37 @@
+package com.epicode.esercizio;
+
+public class Customer {
+	
+	long id;
+	String name;
+	Integer tier;
+	
+	public Customer(long id, String name, Integer tier) {
+		this.id = id;
+		this.name = name;
+		this.tier = tier;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getTier() {
+		return tier;
+	}
+
+	public void setTier(Integer tier) {
+		this.tier = tier;
+	}
+	
+	
+
+}
---
+++ JavaStreams&LambdaExpressions/src/com/epicode/esercizio/Order.java
@@ -0,0 +1,77 @@
+package com.epicode.esercizio;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+	
+	long id;
+	String status;
+	LocalDate orderDate, deliveryDate;
+	List<Product> products;
+	Customer customer;
+	
+//	constructor
+	public Order(long id, 
+			String status, LocalDate orderDate, 
+			LocalDate deliveryDate, List<Product> products,
+			Customer customer) {
+		
+		super();
+		this.id = id;
+		this.status = status;
+		this.orderDate = orderDate;
+		this.deliveryDate = deliveryDate;
+		this.products = products;
+		this.customer = customer;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDate orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public LocalDate getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(LocalDate deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+}
