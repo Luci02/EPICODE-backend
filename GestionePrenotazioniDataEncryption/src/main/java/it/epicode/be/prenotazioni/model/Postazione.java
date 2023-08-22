a09855e6107252332b3f331439a393aafec1b36c
Luci02 <luciparlato50@gmail.com>
2023-08-22 09:07:40 +0200

    commit

---
+++ GestionePrenotazioniDataEncryption/src/main/java/it/epicode/be/prenotazioni/model/Postazione.java
@@ -0,0 +1,33 @@
+package it.epicode.be.prenotazioni.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Postazione {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String codice;
+	private String descrizione;
+	private Integer numeroMassimoOccupanti;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoPostazione tipo;
+	
+	@ManyToOne
+	private Edificio edificio;
+	
+//	@OneToMany(mappedBy = "postazione")
+//	@OrderBy(value = "dataPrenotata")
+//	private List<Prenotazione> listaPrenotazioni;
+}
